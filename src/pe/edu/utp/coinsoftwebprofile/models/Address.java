2fbc16300359da78ea182377015806e143ba70f8
KEVIN COTERA BELLO <1330553@utp.edu.pe>
2017-10-21 17:57:21 -0500

    classes address and phone

--- src/pe/edu/utp/coinsoftwebprofile/models/Address.java
+++ src/pe/edu/utp/coinsoftwebprofile/models/Address.java
@@ -1,4 +1,171 @@
 package pe.edu.utp.coinsoftwebprofile.models;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Address {
+    private int idcustomer;
+    private String department;
+    private String province;
+    private String district;
+    private String via;
+    private String name_via;
+    private String number_door;
+    private String number_department;
+    private String block;
+    private String allotment;
+    private String reference;
+    private String status;
+
+    public Address() {
+    }
+
+    public Address(int idcustomer, String department, String province, String district, String via, String name_via,
+                   String number_door, String number_department, String block, String allotment, String reference, String status) {
+        this.idcustomer = idcustomer;
+        this.department = department;
+        this.province = province;
+        this.district = district;
+        this.via = via;
+        this.name_via = name_via;
+        this.number_door = number_door;
+        this.number_department = number_department;
+        this.block = block;
+        this.allotment = allotment;
+        this.reference = reference;
+        this.status = status;
+    }
+
+    public int getIdcustomer() {
+        return idcustomer;
+
+    }
+
+    public Address setIdcustomer(int idcustomer) {
+        this.idcustomer = idcustomer;
+        return this;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public Address setDepartment(String department) {
+        this.department = department;
+        return this;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public Address setProvince(String province) {
+        this.province = province;
+        return this;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public Address setDistrict(String district) {
+        this.district = district;
+        return this;
+    }
+
+    public String getVia() {
+        return via;
+    }
+
+    public Address setVia(String via) {
+        this.via = via;
+        return this;
+    }
+
+    public String getName_via() {
+        return name_via;
+    }
+
+    public Address setName_via(String name_via) {
+        this.name_via = name_via;
+        return this;
+    }
+
+    public String getNumber_door() {
+        return number_door;
+    }
+
+    public Address setNumber_door(String number_door) {
+        this.number_door = number_door;
+        return this;
+    }
+
+    public String getNumber_department() {
+        return number_department;
+    }
+
+    public Address setNumber_department(String number_department) {
+        this.number_department = number_department;
+        return this;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public Address setBlock(String block) {
+        this.block = block;
+        return this;
+    }
+
+    public String getAllotment() {
+        return allotment;
+    }
+
+    public Address setAllotment(String allotment) {
+        this.allotment = allotment;
+        return this;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public Address setReference(String reference) {
+        this.reference = reference;
+        return this;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Address setStatus(String status) {
+        this.status = status;
+        return this;
+    }
+
+    public static Address from(ResultSet rs) {
+        try {
+            return new Address(
+                    rs.getInt("idcustomer"),
+                    rs.getString("department"),
+                    rs.getString("province"),
+                    rs.getString("district"),
+                    rs.getString("via"),
+                    rs.getString("name_via"),
+                    rs.getString("number_door"),
+                    rs.getString("number_department"),
+                    rs.getString("block"),
+                    rs.getString("allotment"),
+                    rs.getString("reference"),
+                    rs.getString("status")
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+
 }
--- src/pe/edu/utp/coinsoftwebprofile/models/Phone.java
+++ src/pe/edu/utp/coinsoftwebprofile/models/Phone.java
@@ -1,5 +1,83 @@
 package pe.edu.utp.coinsoftwebprofile.models;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Phone {
+    private int idphone;
+    private String number;
+    private String type;
+    private String operator;
+    private String status;
 
+    public Phone() {
+    }
+
+    public Phone(int idphone, String number, String type, String operator, String status) {
+        this.idphone = idphone;
+        this.number = number;
+        this.type = type;
+        this.operator = operator;
+        this.status = status;
+    }
+
+    public int getIdphone() {
+        return idphone;
+    }
+
+    public Phone setIdphone(int idphone) {
+        this.idphone = idphone;
+        return this;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public Phone setNumber(String number) {
+        this.number = number;
+        return this;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Phone setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public Phone setOperator(String operator) {
+        this.operator = operator;
+        return this;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Phone setStatus(String status) {
+        this.status = status;
+        return this;
+    }
+
+    public static Phone from(ResultSet rs) {
+        try {
+            return new Phone(
+                    rs.getInt("idphone"),
+                    rs.getString("number"),
+                    rs.getString("type"),
+                    rs.getString("operator"),
+                    rs.getString("status")
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
 }
