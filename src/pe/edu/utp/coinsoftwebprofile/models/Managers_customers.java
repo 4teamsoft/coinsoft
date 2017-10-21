07e0b134069d2421c5980fca24cc393c9f09907c
Felix Santiago Vargas Reyes <1330187@utp.edu.pe>
2017-10-21 14:42:36 -0500

    Class Entity update 4

--- src/pe/edu/utp/coinsoftwebprofile/models/Managers_customers.java
+++ src/pe/edu/utp/coinsoftwebprofile/models/Managers_customers.java
@@ -1,4 +1,30 @@
 package pe.edu.utp.coinsoftwebprofile.models;
 
 public class Managers_customers {
+
+    private int idmanager;
+    private int idcustomer;
+
+
+    public int getIdmanager() {
+        return idmanager;
+    }
+
+    public Managers_customers setIdmanager(int idmanager) {
+        this.idmanager = idmanager;
+        return this;
+    }
+
+    public int getIdcustomer() {
+        return idcustomer;
+    }
+
+    public Managers_customers setIdcustomer(int idcustomer) {
+        this.idcustomer = idcustomer;
+        return this;
+    }
+
+
+
+
 }
